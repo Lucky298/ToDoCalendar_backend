a7cbe4bb5b7370d4b4c8e8a734c8cbf2a19eb6fe
mugiwar <mugiwar8@gmail.com>
2022-06-09 14:21:01 +0300

    install flake8

--- ToDoCalendar/ToDoCalendar/settings.py
+++ ToDoCalendar/ToDoCalendar/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_u4n888^^rr2n9!p@#*&9y1ak9)08e0lln7*bk@4=skp0yd__f'
+SECRET_KEY = 'django-insecure-_xu4n888^^rr2n9!p@#*&9y1ak9)08e0lln7*bk@4' \
+             '=skp0yd__f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,16 +94,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
 
